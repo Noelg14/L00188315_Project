08ea792e2fb84ee3e43bd8939840077b478be428
Noel Griffin <noel@noelgriffin.ie>
2025-04-07 14:43:29 +0100

    update always on to False for free tier

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,7 @@ resource "azurerm_service_plan" "atu-sp" {
   resource_group_name = azurerm_resource_group.rg-atu.name
   os_type             = "Linux"
   sku_name            = "F1"
+
 }
 
 
@@ -27,7 +28,9 @@ resource "azurerm_linux_web_app" "ob-dash" {
   resource_group_name = azurerm_resource_group.rg-atu.name
   service_plan_id     = azurerm_service_plan.atu-sp.id
 
+
   site_config {
+    always_on           = false
     application_stack {
       dotnet_version =  "8.0"
     }
