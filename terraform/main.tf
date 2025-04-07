828a55ec12f7d70cb4942527d143fee407985067
Noel Griffin <griffin.noel1@gmail.com>
2025-04-07 08:24:55 +0100

    Fix issue with underscore in name

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_service_plan" "atu-sp" {
 }
 
 
-resource "azurerm_linux_web_app" "ob_dash" {
+resource "azurerm_linux_web_app" "ob-dash" {
   name                = "${var.prefix}-dashboard"
   location            = azurerm_resource_group.rg-atu.location
   resource_group_name = azurerm_resource_group.rg-atu.name
