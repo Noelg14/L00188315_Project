b1fc079c5a13c1cf8e751ceecea809b2493dd245
Noel Griffin <griffin.noel1@gmail.com>
2025-04-07 08:30:20 +0100

    update output with new name

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,9 +2,9 @@
 # SPDX-License-Identifier: MPL-2.0
 
 output "app_name" {
-  value = azurerm_linux_web_app.ob_dash.name
+  value = azurerm_linux_web_app.ob-dash.name
 }
 
 output "app_url" {
-  value = "https://${azurerm_linux_web_app.ob_dash.default_hostname}"
+  value = "https://${azurerm_linux_web_app.ob-dash.default_hostname}"
 }
