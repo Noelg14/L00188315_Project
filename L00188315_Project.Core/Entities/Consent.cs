3d4daefc9c21b7b233084ad2187b215ddd6e2a7a
Noel Griffin <noel@noelgriffin.ie>
2025-05-20 22:01:38 +0100

    Add Last comments

--- L00188315_Project.Core/Entities/Consent.cs
+++ L00188315_Project.Core/Entities/Consent.cs
@@ -7,19 +7,51 @@ using Microsoft.AspNetCore.Identity;
 
 namespace L00188315_Project.Core.Entities
 {
+    /// <summary>
+    /// Consent entity representing the user's consent for data sharing.
+    /// </summary>
     public class Consent
     {
+        /// <summary>
+        /// ID of the consent.
+        /// </summary>
         public required string ConsentId { get; set; }
+        /// <summary>
+        /// User ID associated with the consent.
+        /// </summary>
         public string? UserId { get; set; }
+        /// <summary>
+        /// Scopes of the consent.
+        /// </summary>
         public string? Scopes { get; set; }
+        /// <summary>
+        /// Provider of the consent.
+        /// </summary>
         public string? Provider { get; set; }
+        /// <summary>
+        /// Conent status.
+        /// </summary>
         public ConsentStatus? ConsentStatus { get; set; } = Entities.ConsentStatus.Created;
+        /// <summary>
+        /// List of accounts associated with the consent.
+        /// </summary>
         public List<Account>? Account { get; set; }
+        /// <summary>
+        /// Date and time when the consent was created.
+        /// </summary>
         public DateTime? Created { get; set; } = DateTime.Now;
+        /// <summary>
+        ///  Date and time when the consent was last updated.
+        /// </summary>
         public DateTime? Updated { get; set; }
+        /// <summary>
+        /// Da and time when the consent expires.
+        /// </summary>
         public DateTime? Expires { get; set; }
     }
-
+    /// <summary>
+    /// Enum representing the status of the consent.
+    /// </summary>
     public enum ConsentStatus
     {
         Created,
--- L00188315_Project.Core/Entities/Transaction.cs
+++ L00188315_Project.Core/Entities/Transaction.cs
@@ -6,23 +6,74 @@ using System.Threading.Tasks;
 
 namespace L00188315_Project.Core.Entities
 {
+    /// <summary>
+    /// Transaction entity representing a financial transaction associated with a bank account.
+    /// </summary>
     public class Transaction
     {
+        /// <summary>
+        /// ID of the transaction.
+        /// </summary>
         public string Id { get; set; } = Guid.NewGuid().ToString();
+        /// <summary>
+        /// Account Id associated with the transaction.
+        /// </summary>
         public string? RootAccountId { get; set; }
+        /// <summary>
+        /// Account associated with the transaction.
+        /// </summary>
         public Account? Account { get; set; } = null!;
+        /// <summary>
+        /// Amount of the transaction.
+        /// </summary>
         public string? Amount { get; set; }
+        /// <summary>
+        /// Amount currency of the transaction.
+        /// </summary>
         public string? AmountCurrency { get; set; }
+        /// <summary>
+        /// Credit or debit indicator of the transaction.
+        /// </summary>
         public string? CreditDebitIndicator { get; set; }
+        /// <summary>
+        /// date and time when the transaction was created.
+        /// </summary>
         public DateTime? BookingDateTime { get; set; }
+        /// <summary>
+        /// Value date and time of the transaction.
+        /// </summary>
         public DateTime? ValueDateTime { get; set; }
+        /// <summary>
+        /// Creditor name associated with the transaction.
+        /// </summary>
         public string? CreditorAccount { get; set; }
+        /// <summary>
+        /// Debitor name associated with the transaction.
+        /// </summary>
         public string? DebtorAccount { get; set; }
+        /// <summary>
+        /// Proprietary bank transaction code associated with the transaction.
+        /// </summary>
         public string? ProprietaryBankTransactionCode { get; set; }
+        /// <summary>
+        /// Proprietary bank transaction issuer associated with the transaction.
+        /// </summary>
         public string? ProprietaryBankTransactionIssuer { get; set; }
+        /// <summary>
+        /// Status of the transaction.
+        /// </summary>
         public string? Status { get; set; }
+        /// <summary>
+        /// Transaction Id associated with the transaction - from the bank
+        /// </summary>
         public string? TransactionId { get; set; }
+        /// <summary>
+        /// Transaction information 
+        /// </summary>
         public string? TransactionInformation { get; set; }
+        /// <summary>
+        /// User comments associated with the transaction.
+        /// </summary>
         public string? UserComments { get; set; }
     }
 }
